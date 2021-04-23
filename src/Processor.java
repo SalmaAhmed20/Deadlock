fd7bf751a13603b0bbee98f12ae3eee822a3ee90
SalmaAhmed20 <64385957+SalmaAhmed20@users.noreply.github.com>
2021-04-23 20:55:54 +0200

    main 1

--- src/Processor.java
+++ src/Processor.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
+import static java.lang.System.exit;
+
 public class Processor {
     public static void main( String[] args ) {
+        //test case
+        //****
+        //** request algorithm
+        int option ;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("1- process request resources");
+            System.out.println("2- process release resources");
+            System.out.println("3- Quit");
+            option = sc.nextInt();
+            switch (option)
+            {
+                case 1://
+                {
+                    break;
+                }
+                case 2://
+                {
+                    break;
+                }
+                case 3:
+                    exit(0);
+                default:
+                    System.out.println("Not valid");
+            }
+        }while (true);
 
     }
 }
